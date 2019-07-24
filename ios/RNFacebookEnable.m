382a91d069353d24ec18e50e03cd489cf52f5253
Yann Bartel <ybartel@micropole.com>
2019-07-24 11:30:20 +0200

    Fixing ios methods

--- ios/RNFacebookEnable.m
+++ ios/RNFacebookEnable.m
@@ -14,7 +14,7 @@ RCT_EXPORT_METHOD(setEnabler:(BOOL)value)
     [[NSUserDefaults standardUserDefaults] setBool:value forKey:USER_DEFAULTS_KEY];
 }
 
-RCT_EXPORT_METHOD(isEnabled:(BOOL)value result:(RCTResponseSenderBlock)callback)
+RCT_EXPORT_METHOD(isEnabled:(RCTResponseSenderBlock)callback)
 {
     BOOL result = [[NSUserDefaults standardUserDefaults] boolForKey:USER_DEFAULTS_KEY];
     callback(@[[NSNull null], [NSNumber numberWithBool:result]]);
@@ -23,12 +23,7 @@ RCT_EXPORT_METHOD(isEnabled:(BOOL)value result:(RCTResponseSenderBlock)callback)
 RCT_EXPORT_METHOD(hasValue:(RCTResponseSenderBlock)callback)
 {
     id value = [[NSUserDefaults standardUserDefaults] objectForKey:USER_DEFAULTS_KEY];
-    NSNumber *result;
-    if (value != nil) {
-        result = [NSNumber numberWithBool:YES];
-    } else {
-        result = [NSNumber numberWithBool:NO];
-    }
+    NSNumber *result = [NSNumber numberWithBool:value != nil];
     callback(@[[NSNull null], result]);
 }
 
