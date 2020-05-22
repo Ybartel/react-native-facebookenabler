c604d7b5b486d9e52fde69fa8fd6470a6b3c7b92
Yann Bartel <ybartel@micropole.com>
2020-05-22 10:12:14 +0200

    replacing project structure with most recent one

--- android/src/main/java/com/reactlibraryfacebookenable/RNFacebookEnableModule.java
+++ android/src/main/java/com/reactlibraryfacebookenable/RNFacebookEnableModule.java
@@ -1,13 +1,10 @@
 
-package com.reactlibrary;
-
-import android.content.Context;
-import android.content.SharedPreferences;
+package com.reactlibraryfacebookenable;
 
 import com.facebook.react.bridge.ReactApplicationContext;
 import com.facebook.react.bridge.ReactContextBaseJavaModule;
 import com.facebook.react.bridge.ReactMethod;
-import com.facebook.react.bridge.Promise;
+import com.facebook.react.bridge.Callback;
 
 public class RNFacebookEnableModule extends ReactContextBaseJavaModule {
 
--- android/src/main/java/com/reactlibraryfacebookenable/RNFacebookEnablePackage.java
+++ android/src/main/java/com/reactlibraryfacebookenable/RNFacebookEnablePackage.java
@@ -1,5 +1,5 @@
 
-package com.reactlibrary;
+package com.reactlibraryfacebookenable;
 
 import java.util.Arrays;
 import java.util.Collections;
